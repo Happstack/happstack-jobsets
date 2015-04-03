def497a8506b13366bcfc7f1cdffe5d845149a59
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-02 23:14:09 -0500

    another attempt at fetching from git

--- happstack-server-git-head.nix
+++ happstack-server-git-head.nix
@@ -1,6 +1,6 @@
 { mkDerivation, base, base64-bytestring, blaze-html, bytestring
 , containers, directory, exceptions, extensible-exceptions
-, fetchgit, filepath, hslogger, html, HUnit, monad-control, mtl
+, filepath, hslogger, html, HUnit, monad-control, mtl
 , network, network-uri, old-locale, parsec, process, sendfile
 , stdenv, syb, system-filepath, template-haskell, text, threads
 , time, time-compat, transformers, transformers-base
@@ -9,11 +9,7 @@
 mkDerivation {
   pname = "happstack-server";
   version = "7.4.2";
-  src = fetchgit {
-    url = "https://github.com/Happstack/happstack-server.git";
-    sha256 = "97b51428b61ab373d0d7d86c9240dca86fbbea6692c92ec57624cade1bb0228b";
-    rev = "030f4013e6ba2b71ec87921474ae6cf38c751caa";
-  };
+  src = happstack-server-git;
   buildDepends = [
     base base64-bytestring blaze-html bytestring containers directory
     exceptions extensible-exceptions filepath hslogger html
