ef3f642eed6d7fbf76943b65c02926060b4d8f02
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-07 18:21:05 -0500

    added happstack-server-tls

--- release.nix
+++ release.nix
@@ -5,6 +5,12 @@ let
       let pkgs = import <nixpkgs> { inherit system; };
       in
        pkgs.haskellngPackages.callPackage <happstack-server-git> {};
+
+    happstackServerTlsGit =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage <happstack-server-tls-git> {};
   };
 in
   jobs
