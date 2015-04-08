83470eb52f5d82b98299acc02add831ae1e1a887
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-07 22:19:17 -0500

    added some web-routes targets

--- release.nix
+++ release.nix
@@ -24,25 +24,49 @@ let
       in
        pkgs.haskellngPackages.callPackage <happstack-jmacro-git> {};
 
-   reform-git =
+    reform-git =
       { system ? builtins.currentSystem }:
       let pkgs = import <nixpkgs> { inherit system; };
       in
        pkgs.haskellngPackages.callPackage (<reform-git> + /reform) {};
 
-   reform-hsp-git =
+    reform-hsp-git =
       { system ? builtins.currentSystem }:
       let pkgs = import <nixpkgs> { inherit system; };
       in
        pkgs.haskellngPackages.callPackage (<reform-git> + /reform-hsp) {};
 
-   reform-happstack-git =
+    reform-happstack-git =
       { system ? builtins.currentSystem }:
       let pkgs = import <nixpkgs> { inherit system; };
       in
        pkgs.haskellngPackages.callPackage (<reform-git> + /reform-happstack) {};
 
-  };
+    web-routes-git =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage (<web-routes-git> + /web-routes) {};
+
+    web-routes-boomerang-git =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage (<web-routes-git> + /web-routes-boomerang) {};
+
+   web-routes-happstack-git =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage (<web-routes-git> + /web-routes-happstack) {};
+
+   web-routes-hsp-git =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage (<web-routes-git> + /web-routes-hsp) {};
+
+   };
 in
   jobs
 
