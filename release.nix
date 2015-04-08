5ef10c3f1fdb34932a89336f4c0468a4487e0785
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-07 20:00:53 -0500

    added happstack-hsp-git and happstack-jmacro-git, changed named of jobs to not use camelCase

--- release.nix
+++ release.nix
@@ -1,16 +1,29 @@
 let
   jobs = rec {
-    happstackServerGit =
+    happstack-server-git =
       { system ? builtins.currentSystem }:
       let pkgs = import <nixpkgs> { inherit system; };
       in
        pkgs.haskellngPackages.callPackage <happstack-server-git> {};
 
-    happstackServerTlsGit =
+    happstack-server-tls-git =
       { system ? builtins.currentSystem }:
       let pkgs = import <nixpkgs> { inherit system; };
       in
        pkgs.haskellngPackages.callPackage <happstack-server-tls-git> {};
+
+    happstack-hsp-git =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage <happstack-hsp-git> {};
+
+    happstack-jmacro-git =
+      { system ? builtins.currentSystem }:
+      let pkgs = import <nixpkgs> { inherit system; };
+      in
+       pkgs.haskellngPackages.callPackage <happstack-jmacro-git> {};
+
   };
 in
   jobs
